a0522ef6f1584e567fe9ce37fe45c554f7004868
Juan Barcelos <juanbarcelos@outlook.com.br>
2025-10-28 11:44:19 -0300

    feat: add header component

--- src/app/pages/layout/layout.ts
+++ src/app/pages/layout/layout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../../components/sidebar/sidebar";
+import { Header } from "../../components/header/header";
 
 @Component({
   selector: 'app-layout',
-  imports: [Sidebar],
+  imports: [Sidebar, Header],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
