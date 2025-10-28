76727f7ce9a1248cb54e9e561108e5afcd659cb4
Juan Barcelos <juanbarcelos@outlook.com.br>
2025-10-28 09:55:34 -0300

    login page

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PrimaryButton } from "../../components/primary-button/primary-button";
+
+@Component({
+  selector: 'app-login',
+  imports: [PrimaryButton],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+
+}
